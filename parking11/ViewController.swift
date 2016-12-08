1f4359f356e2cce6421fd981edaf1611d3ad9f70
ftwdjw <ftwdjw@gmail.com>
2016-12-07 20:14:22 -0600

    initial commit

--- parking11/ViewController.swift
+++ parking11/ViewController.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    lazy var locationManager : CLLocationManager = {
+        
+        let loc = CLLocationManager()
+        
+        //Set up location manager with defaults
+        loc.desiredAccuracy = kCLLocationAccuracyBest
+        loc.distanceFilter = kCLDistanceFilterNone
+        loc.delegate = self
+        
+        //Optimisation of battery
+        loc.pausesLocationUpdatesAutomatically = true
+        loc.activityType = CLActivityType.fitness
+        loc.allowsBackgroundLocationUpdates = false
+        
+        return loc
+    }()
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestAlwaysAuthorization()
+        //Map
+        mapView.showsUserLocation = true
+        mapView.mapType = MKMapType.hybrid
+        mapView.userTrackingMode = .follow
+        mapView.showsTraffic = true
+        mapView.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == CLAuthorizationStatus.authorizedAlways {
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = MKUserTrackingMode.follow
+        } else {
+            print("Permission Refused")
+        }
     }
 
 
